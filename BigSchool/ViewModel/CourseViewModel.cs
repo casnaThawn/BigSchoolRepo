da076a6f08a1111492bdfa82c220cbb7f976a27a
phamdinhthang <dinhthang05112002@gmail.com>
2023-03-14 22:14:26 +0700

    Update BigSchool project week 2

--- BigSchool/ViewModel/CourseViewModel.cs
+++ BigSchool/ViewModel/CourseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using BigSchool.Models;
+using Microsoft.Build.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,24 @@ namespace BigSchool.ViewModel
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required] 
         public byte Category { get; set; }
+
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime() 
         {
-            return DateTime.Parse(string.Format("{0}","{1}", Date, Time)); 
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time)); 
         }
     }
 }
